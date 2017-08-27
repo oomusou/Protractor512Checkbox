ea3aaf4617cea289776e08b00fc80bdf3459d093
Oomusou <oomusou@gmail.com>
2017-08-27 10:17:22 +0800

    Acceptance Test (failed)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -5,10 +5,38 @@ describe('protractor512-checkbox App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it(`should have '3' labels`, () => {
+    expect(page.getLabelCount()).toBe(3);
+  });
+
+  it(`should have '3' checkboxes`, () => {
+    expect(page.getCheckboxCount()).toBe(3);
+  });
+
+  it(`should have '0' checkbox selected by default`, () => {
+    expect(page.getSelectedCloudCount()).toBe('0');
+  });
+
+  it(`should show '1' when 'AWS' selected`, () => {
+    page.selectCloudByIndex(0);
+    expect(page.getSelectedCloudCount()).toBe('1');
+  });
+
+  it(`should have '2' when 'AWS' and 'Azure' selected`, () => {
+    page
+      .selectCloudByText('AWS')
+      .selectCloudByText('Azure');
+    expect(page.getSelectedCloudCount()).toBe('2');
+  });
+
+  it(`should '3' when all checkbox selected`, () => {
+    page
+      .selectCloudByText('AWS')
+      .selectCloudByText('Azure')
+      .selectCloudByText('GCP');
+    expect(page.getSelectedCloudCount()).toBe('3');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,29 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getLabelCount(): any {
+    return element.all(by.tagName('label')).count();
+  }
+
+  getCheckboxCount(): any {
+    return element.all(by.css('input[type="checkbox"]')).count();
+  }
+
+  selectCloudByIndex(index: number): AppPage {
+    element.all(by.css('label[name="cloud"]'))
+      .get(index)
+      .click();
+
+    return this;
+  }
+
+  selectCloudByText(text: string): AppPage {
+    element(by.cssContainingText('label', text)).click();
+
+    return this;
+  }
+
+  getSelectedCloudCount(): any {
+    return element(by.css('p')).getText();
   }
 }
